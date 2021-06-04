084a5297937c36ff7e728358701dd15c689c3d45
Ed Hubbard <Edhubbard1994@gmail.com>
2021-06-03 22:56:23 -0500

    led blink start

---
+++ src/test.c
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#define PORT_OUT *( (uint32_t*) 0x41008110)
+#define PORT_DIR_SET *( (uint32_t*) 0x41008108)
+#define PORT_PIN_FUNCTION *( (uint32_t*) 0x41008110)
+//uint32_t *pcclear = 0x4100800;
+
+void delay (unsigned int time) {
+    for (int k =0; k < time; k++) {
+        asm("nop");
+    }
+}
+
+
+int main(void){
+    PORT_OUT = (1 << 18);
+    PORT_DIR_SET = (1 << 18);
+    for(;;){
+        
+        delay(2000);
+        PORT_OUT =  (0 << 18);
+        delay(2000);
+        PORT_OUT = (1 << 18);
+        //0x41008000
+        //0x41008010
+    }
+}
+
+
+
+void Reset_Handler() {
+    main();
+
+}
