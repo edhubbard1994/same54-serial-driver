3ae392d4e8297a319099f3ebbb2175693d916599
Ed Hubbard <Edhubbard1994@gmail.com>
2021-06-06 19:15:13 -0500

    led flash

--- src/test.c
+++ src/test.c
@@ -2,16 +2,33 @@
 //#include "atmel_start.h"
 
 //#include "setup.h"
-extern _estack;
 
+
+extern _estack; // stack pointer reference from linker
+
+
+
+typedef struct { // define vector table type 
+    uint32_t*       stack_ptr // initialize stack pointer
+
+} VectorTable;
+
+
+// create the vector table and link it to .vectors section of memory
 __attribute__ ((section(".vectors")))
-uint32_t RESET_VECTOR_ARR[64];
+const VectorTable vectors = {
+    .stack_ptr = &_estack
+};
 
-#define PORT_OUT *( (uint32_t*) 0x41008110)
-#define PORT_DIR_SET *( (uint32_t*) 0x41008108)
-#define PORT_PIN_FUNCTION *( (uint32_t*) 0x41008110)
+
+// Setup PORT register for LED flashing
+#define PORT_OUT *( (uint32_t*) 0x41008110) // Direct write to PC18 
+#define PORT_DIR_SET *( (uint32_t*) 0x41008108) // Direction set for PC18
+#define PORT_PIN_FUNCTION *( (uint32_t*) 0x41008110) // Mode register for PORT C
 //uint32_t *pcclear = 0x4100800;
 
+
+// Simple Delay Function
 void delay (unsigned int time) {
     for ( int k =0; k < time; k++) {
         asm("nop");
@@ -20,15 +37,14 @@ void delay (unsigned int time) {
 
 
 int main(void){
-    //system_init();
-    //init_board();
+
     PORT_OUT = (1 << 18);
     PORT_DIR_SET = (1 << 18);
     for(;;){
         
-        delay(2000);
+        delay(200000);
         PORT_OUT =  (0 << 18);
-        delay(2000);
+        delay(200000);
         PORT_OUT = (1 << 18);
         //0x41008000
         //0x41008010
@@ -38,10 +54,7 @@ int main(void){
 
 
 void Reset_Handler() {
-    uint32_t *stack_start;
-    stack_start = &RESET_VECTOR_ARR[0];
-    stack_start = &_estack;
-
+    
     asm("mov r0,0\n"
         "ldr r0,[r0]\n"
         "mov sp,r0\n");
