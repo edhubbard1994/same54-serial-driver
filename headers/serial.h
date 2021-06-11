74403495cdf89a2a1adb53bd840e4aecde8bf8e2
Ed Hubbard <Edhubbard1994@gmail.com>
2021-06-10 19:03:15 -0500

    serial start

---
+++ headers/serial.h
@@ -0,0 +1,11 @@
+#ifndef __SERIAL_H
+#define __SERIAL_H
+
+
+void serial_write(const char *message);
+
+const char *serial_read();
+
+
+
+#endif
---
+++ src/serial.c
@@ -0,0 +1,12 @@
+#include "serial.h"
+
+
+void serial_write(const char *message){
+
+}
+
+
+
+const char *serial_read() {
+    return (char *) 0;
+}
