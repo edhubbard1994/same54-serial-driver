ad076ed18209a76c77fd902b1b2c754eded957e7
Ed Hubbard <Edhubbard1994@gmail.com>
2021-06-26 18:33:00 -0500

    Serial Register definitions

--- headers/serial.h
+++ headers/serial.h
@@ -1,6 +1,8 @@
 #ifndef __SERIAL_H
 #define __SERIAL_H
 
+#include <stdint.h>
+
 /**
  * @brief Header for the Serial Interface 
  * 
@@ -14,6 +16,13 @@
 //addresses for sercom functions
 #define SERCOM0 0x40003000
 #define SERCOM1 0x40003400
+
+#define CTRLA0 *((uint32_t *) 0x40003000)
+#define CTRLB0 *((uint32_t *) 0x40003004)
+#define CTRLC0 *((uint32_t *) 0x40003008)
+#define BAUD0 *((uint32_t *) 0x4000300C)
+#define LENGTH0 *((uint32_t *) 0x40003022)
+#define DATA0 *((uint32_t *) 0x40003028)
 
 
 //TODO: Make this a configurable mode for user
