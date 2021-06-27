c7a11a4e18d56a29dbad3531b482c4c275333a31
Ed Hubbard <Edhubbard1994@gmail.com>
2021-06-26 19:05:03 -0500

    Ctrla register set

--- src/serial.c
+++ src/serial.c
@@ -4,9 +4,20 @@
 
 
 void serial_setup() {
-    uint32_t *ctrl_a = *((u_int32_t *) SERCOMM1);
-    ctrl_a = 0x01;
+
+    CTRLA0 = 0x01; // Reset all bits
+    CTRLA0 |= 0x02; // enable 
+    CTRLA0 |= 0x04; // internal clock
+    CTRLA0 |= 0x10000000; //asyncronous (UART) mode
+
+    CTRLB0 |= 0x100; //collision detection
+
+
+
+
+
     
+
 
 }
 
