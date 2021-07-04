2030d24bf2b3a3226ff64f837ace6440d95ab120
Ed Hubbard <Edhubbard1994@gmail.com>
2021-07-04 14:01:28 -0500

    PCHCTRL register clock to sercom

--- headers/clock.h
+++ headers/clock.h
@@ -6,6 +6,9 @@
 #define OSCCTL *((uint32_t*) 0x40001000)
 #define OSC32KCTRL *((uint32_t*) 0x40001400)
 
+#define GCLK *((uint32_t*) 0x40001C00)
+#define PCHCTRL0 *((uint32_t*) 0x40001C80)
+
 void init_clock();
  
 
