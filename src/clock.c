242d93452b3c4aa79a46f7137ac49eab86e3b335
Ed Hubbard <Edhubbard1994@gmail.com>
2021-07-11 15:19:10 -0500

    enable periphrial clocks

--- src/clock.c
+++ src/clock.c
@@ -10,10 +10,14 @@ void init_clock() {
     uint32_t *generator0 = &PCHCTRL0; //Set generator0 settings
     uint32_t *sercom0 = &PCHCTRL47[7]; //get register for SERCOM0
     
-    sercom0 = 0x00; //Set generator clock source to clock 0
+   
     *generator0 |= 0x800; //output enable
     *generator0 |= 0x100; //generator enable
     *generator0 |= 0x0; // generator source XOSC0
+    *generator0 |= 10000; // set division factor to 1 (raw clock input)
+
+    *sercom0 |= 0x00; //Set generator clock source to clock 0
+    *sercom0 |= 0x40; //enabel the periphrial channel
 
 
 
