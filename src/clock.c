17a91af6957b1cb679e795eb2243b8985b3fd98b
Ed Hubbard <Edhubbard1994@gmail.com>
2021-07-11 13:52:04 -0500

    clock setup

--- src/clock.c
+++ src/clock.c
@@ -7,8 +7,8 @@ void init_clock() {
 
 
 
-    uint32_t *generator0 = PCHCTRL0; //Set generator0 settings
-    uint32_t *sercom0 = PCHCTRL47[7]; //get register for SERCOM0
+    uint32_t *generator0 = &PCHCTRL0; //Set generator0 settings
+    uint32_t *sercom0 = &PCHCTRL47[7]; //get register for SERCOM0
     
     sercom0 = 0x00; //Set generator clock source to clock 0
     *generator0 |= 0x800; //output enable
