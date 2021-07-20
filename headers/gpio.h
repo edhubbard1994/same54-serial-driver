7534999565f482ce7b8e80bc9d2ea9371147a163
Ed Hubbard <Edhubbard1994@gmail.com>
2021-07-19 23:11:21 -0500

    pin config

--- headers/gpio.h
+++ headers/gpio.h
@@ -10,7 +10,6 @@ typedef unsigned int pin_num_t;
 #define PORT_BASE_VALUE 0x41008000
 
 
-
 //TODO: all of these definitions for PC18. Make all of these generic offset values 
 
 #define PORT_OUT *( (uint32_t*) 0x41008110) // Direct write to PC18 
@@ -18,8 +17,9 @@ typedef unsigned int pin_num_t;
 #define PORT_PIN_FUNCTION *( (uint32_t*) 0x41008110) // Mode register for PORT C
 #define PINPMUX *( (uint32_t*) 0x41008130)
 #define PINCFG0 *( (uint32_t*) 0x41008140)
-#define PINCFG31 *( (uint32_t*) 0x4100815f)
+#define PINCFG31 *( (uint32_t*) 0x4100815f) // completely irrelevant 
 
+//New Dynamic Port Config
 #define PORT_A  *( (uint32_t*) 0x41008000)
 #define PORT_B  *( (uint32_t*) 0x41008080)
 #define PORT_C  *( (uint32_t*) 0x41008100)
@@ -27,6 +27,8 @@ typedef unsigned int pin_num_t;
 
 #define PIN_DIRECTION(port,pin) *(port + 0x08) |= (0x1 << pin)
 #define PIN_WRITE(port,pin,state) *(port + 0x10) |= (state << pin)
+#define PIN_CONFIG(port,pin,state)  *(port + 0x140 + pin) |= (state)
+
 
 //uint32_t *pcclear = 0x4100800;
 
