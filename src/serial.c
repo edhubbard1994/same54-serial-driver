b200458c2310f16c67810230023fe1b74fc81473
Ed Hubbard <Edhubbard1994@gmail.com>
2021-08-05 18:03:40 -0500

    ioset issues

--- src/serial.c
+++ src/serial.c
@@ -52,6 +52,8 @@ const char *serial_read() {
 }
 
 void serial_setup(DriverNumber driver_number,gpio_port_t rx_port,pin_num_t rx_pin) {
+    //FOR TESTING USE SERCOM1 pc22-23 / pad 0-1 in IOSet 2 
+
     CTRLA(driver_number) = 0x01; // Reset all bits
     CTRLA(driver_number) |= 0x02; // enable 
     CTRLA(driver_number) |= 0x04; // internal clock
@@ -60,6 +62,9 @@ void serial_setup(DriverNumber driver_number,gpio_port_t rx_port,pin_num_t rx_pi
     CTRLB(driver_number)|= 0x100; //collision detection
     CTRLB(driver_number) |= 0x10000; //tx enable
     CTRLB(driver_number) |= 0x20000; //rx enable
+    BAUD(driver_number) |= 0x20000; // bogus baud rate
+
+    gpio_multiplex_mode(rx_port,rx_pin,D); //TEST PA07
     
 
 }
